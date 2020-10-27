3c358a3ba6d8b0e4e436056b9122657cf0697a00
John Wisneski <john.j.wisneski@gmail.com>
2020-10-27 07:49:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
-  numbers = [1,2,3]
-  numbers.each do |numbs|
-    #{numbs * numbs}
+ 
   end
   
 end
+
+ numbers = [1,2,3]
+  numbers.each do |numbs|
+    #{numbs * numbs}
